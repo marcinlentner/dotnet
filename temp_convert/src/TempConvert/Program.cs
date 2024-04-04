f4acc4819b10242a02c41f5d77f65cba62038f8e
Marcin Lentner <marcin_len@yahoo.co.uk>
2024-04-04 18:59:38 +0100

    Add fixed point format to float number

--- temp_convert/src/TempConvert/Program.cs
+++ temp_convert/src/TempConvert/Program.cs
@@ -5,4 +5,6 @@ int fahrenheit = 94;
 float celsius = (fahrenheit - 32) * 5 / (float)9;
 
 // show result
-Console.WriteLine($"The temperature is {celsius} Celsius.");
+// use fixed point format:
+// https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+Console.WriteLine($"The temperature is {celsius:F2} Celsius.");
