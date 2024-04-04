ff2306b51bc3389468362b0d68b79c272a23631d
Marcin Lentner <marcin_len@yahoo.co.uk>
2024-04-04 18:46:59 +0100

    Build simple temperature convertion program

---
+++ temp_convert/src/TempConvert/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * Convert fahrenheit to celsius
+ */
+int fahrenheit = 94;
+float celsius = (fahrenheit - 32) * 5 / (float)9;
+
+// show result
+Console.WriteLine($"The temperature is {celsius} Celsius.");
