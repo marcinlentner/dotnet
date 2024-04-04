26bbb975e6f575c5af6fcc7e8c7e27d1b14b57e8
Marcin Lentner <marcin_len@yahoo.co.uk>
2024-04-04 19:10:53 +0100

    Use decimal to convert temperature.

--- temp_convert/src/TempConvert/Program.cs
+++ temp_convert/src/TempConvert/Program.cs
@@ -8,3 +8,7 @@ float celsius = (fahrenheit - 32) * 5 / (float)9;
 // use fixed point format:
 // https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
 Console.WriteLine($"The temperature is {celsius:F2} Celsius.");
+
+// use decimal
+decimal celsius2 = (fahrenheit - 32) * 5 / 9m;
+Console.WriteLine($"More precisely it is {celsius2}.");
